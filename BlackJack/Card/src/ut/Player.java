a242a0ac8507f300ad852d11b52f78af1492ef83
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2020-11-19 14:22:27 +0800

    Update Player.java

--- BlackJack/Card/src/ut/Player.java
+++ BlackJack/Card/src/ut/Player.java
@@ -2,13 +2,22 @@ package ut;
 public class Player 
 {
 	private String name;
-	private int numOfWiner;
+	private int numOfWins;
 	private int token;
 	
 	public void Player(String name)
 	{
 		this.name = name;
-		this.numOfWiner = 0;
+		this.numOfWins = 0;
 		this.token = 0;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	public void setNumOfWins(int num)
+	{
+		this.numOfWins = num;
 	}
 }
