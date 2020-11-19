cec24bb36e3fa66cd57ed2cc083d97695243f847
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2020-11-19 14:32:33 +0800

    Update Card.java

--- BlackJack/Card/src/ut/Card.java
+++ BlackJack/Card/src/ut/Card.java
@@ -2,5 +2,19 @@ package ut;
 
 public class Card 
 {
-	
+	private final String face; 
+   	private final String suit; 
+   	
+   	//no-arg constructor
+   	public Card()
+   	{
+		this.face = "";
+		this.suit = "";
+	}
+   	// four-argument constructor initializes card's face and suit
+ 	  public Card(String face, String suit, int faIval,int suIval)
+  	 {
+ 	    	this.face = face;
+  	    	this.suit = suit; 
+	  }
 }
